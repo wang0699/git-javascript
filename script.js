0759f8aafb023b98eec2ec7a5aaf2098e00563a4
wang zi yue <lololove1993d@gmail.com>
2020-01-13 16:47:16 -0500

    i like coding

---
+++ script.js
@@ -0,0 +1,17 @@
+var name = 'chloe'
+console.log('Hello my name is' + name)
+
+var number = 6
+var result = 0
+
+result = 2 * number
+console.log(result)
+
+result = result * 5
+console.log(result)
+
+result = result / number
+console.log(result)
+
+result = result - 7
+console.log(result)
